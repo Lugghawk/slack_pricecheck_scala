7199d006cb4186fdff99d38284593f2fb64d7bf5
Lugghawk <acidictadpole@gmail.com>
2017-08-19 20:59:27 -0400

    moves the app construction out of the main method

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,12 +8,9 @@ import com.pricecheck.itad._
 object Main extends App{
 
   implicit val system = ActorSystem("bot")
-  override def main(args: Array[String]): Unit = {
-    val slack_client: Client = new SlackClient()
-    val itad_token: String = sys.env("ITAD_TOKEN")
-    val itad_client: ITAD = ITAD(itad_token)
-    val bot:Bot = new Bot(slack_client, itad_client)
-    bot.run()
-
-  }
+  val slack_client: Client = new SlackClient()
+  val itad_token: String = sys.env("ITAD_TOKEN")
+  val itad_client: ITAD = ITAD(itad_token)
+  val bot:Bot = new Bot(slack_client, itad_client)
+  bot.run()
 }
