0059bd9b7338f34a4c11d0b8e5e501b4a7239064
Lugghawk <acidictadpole@gmail.com>
2017-08-21 17:03:25 -0400

    Moved the JS Readers from the ITAD class into the correct companion objects for Price and Shop. Removed the ITAD object

--- src/main/scala/com/pricecheck/itad/isthereanydeal.scala
+++ src/main/scala/com/pricecheck/itad/isthereanydeal.scala
@@ -8,34 +8,14 @@ import com.netaporter.uri.dsl._
 import play.api.libs.functional.syntax._ // Combinator syntax
 
 
-object ITAD {
-  def apply(token: String)(implicit ec: ExecutionContext): ITAD = {
-    new ITAD(token)
-  }
-}
+case class ITAD(token: String)(implicit ec: ExecutionContext){
 
-class ITAD(token: String)(implicit ec: ExecutionContext){
-
-  implicit val shopReader: Reads[Shop] = (
-        (JsPath \ "id").read[String] and
-        (JsPath \ "name").read[String]
-      )(Shop.apply _)
-
-  implicit val priceReader: Reads[Price] = (
-        (JsPath \ "price_new").read[Double] and
-        (JsPath \ "price_old").read[Double] and
-        (JsPath \ "price_cut").read[Double] and
-        (JsPath \ "url").read[String] and
-        (JsPath \ "shop").read[Shop]
-      )(Price.apply _)
 
   def getLowestPrice(gameName: String): Future[Price] = {
     val gamePlain = getPlain(gameName);
-    gamePlain map { plainOption =>
-      plainOption match {
+    gamePlain map {
         case Some(plain) => prices(plain).min
         case None => throw new Exception (s"No price found for $gameName")
-      }
     }
   }
 
@@ -58,10 +38,23 @@ class ITAD(token: String)(implicit ec: ExecutionContext){
 
 }
 
-
+object Shop {
+  implicit val shopReader: Reads[Shop] = (
+        (JsPath \ "id").read[String] and
+        (JsPath \ "name").read[String]
+      )(Shop.apply _)
+}
 case class Shop(id: String, name: String)
 object Price {
   implicit val ord: Ordering[Price] = Ordering.by(_.price_new)
+
+  implicit val priceReader: Reads[Price] = (
+        (JsPath \ "price_new").read[Double] and
+        (JsPath \ "price_old").read[Double] and
+        (JsPath \ "price_cut").read[Double] and
+        (JsPath \ "url").read[String] and
+        (JsPath \ "shop").read[Shop]
+      )(Price.apply _)
 }
 
 case class Price(
